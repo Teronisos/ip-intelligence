7d12ad5359e02fe35e1dccf77a6badedb0b2c33e
Teronisus <jan@braitinger.org>
2025-08-05 19:11:11 +0200

    fix cors problems

--- server/app.js
+++ server/app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*', 
+}));
+
 app.use(express.json());
 
 app.use('/api', require('./routes/ipDataRequest'));
